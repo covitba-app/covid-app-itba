620e0fc0aff9a7dff78162dc404f4a58a8f750d0
ffarall <37149322+ffarall@users.noreply.github.com>
2020-09-03 00:51:55 -0300

    first sketch

--- covid_v1/lib/main.dart
+++ covid_v1/lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:percent_indicator/percent_indicator.dart';
+import 'package:circle_wheel_scroll/circle_wheel_scroll_view.dart';
 
 void main() {
   runApp(MyApp());
@@ -26,7 +28,7 @@ class MyApp extends StatelessWidget {
         // closer together (more dense) than on mobile platforms.
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(title: 'COVITBA'),
     );
   }
 }
@@ -76,6 +78,50 @@ class _MyHomePageState extends State<MyHomePage> {
         // Here we take the value from the MyHomePage object that was created by
         // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
+        backgroundColor: Color.fromARGB(255, 28, 28, 30),
+        shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.vertical(
+          bottom: Radius.circular(30),
+        )),
+        centerTitle: true,
+      ),
+      drawer: Drawer(
+        // Add a ListView to the drawer. This ensures the user can scroll
+        // through the options in the drawer if there isn't enough vertical
+        // space to fit everything.
+        child: ListView(
+          // Important: Remove any padding from the ListView.
+          padding: EdgeInsets.zero,
+          children: <Widget>[
+            UserAccountsDrawerHeader(
+                accountEmail: Text("johndoe@somemail.com"),
+                accountName: Text("John Doe"),
+                arrowColor: Colors.white,
+                currentAccountPicture: CircleAvatar(
+                  backgroundColor: Colors.blue[900],
+                  child: Text("JD"),
+                ),
+                decoration: BoxDecoration(
+                  color: Color.fromARGB(255, 28, 28, 30),
+                )),
+            ListTile(
+                title: Text("Function 1"),
+                onTap: () {
+                  // Update the state of the app.
+                  // ...
+                  // Then close the drawer.
+                  Navigator.pop(context);
+                }),
+            ListTile(
+                title: Text("Function 2"),
+                onTap: () {
+                  // Update the state of the app.
+                  // ...
+                  // Then close the drawer.
+                  Navigator.pop(context);
+                })
+          ],
+        ),
       ),
       body: Center(
         // Center is a layout widget. It takes a single child and positions it
@@ -97,21 +143,57 @@ class _MyHomePageState extends State<MyHomePage> {
           // horizontal).
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+            CircularPercentIndicator(
+              radius: 200.0,
+              animation: true,
+              animationDuration: 1200,
+              lineWidth: 15.0,
+              percent: 0.8,
+              center: Text(
+                "High Exposure",
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20.0),
+                textAlign: TextAlign.center,
+              ),
+              circularStrokeCap: CircularStrokeCap.butt,
+              backgroundColor: Colors.grey,
+              progressColor: Color.fromARGB(255, 150, 2, 10),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+            SafeArea(
+              child: Center(
+                child: Container(
+                  height: 484,
+                  width: 350,
+                  child: CircleListScrollView(
+                    physics: CircleFixedExtentScrollPhysics(),
+                    axis: Axis.horizontal,
+                    itemExtent: 80,
+                    children: List.generate(20, _buildItem),
+                    radius: MediaQuery.of(context).size.width * 0.4,
+                  ),
+                ),
+              ),
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+
+  Widget _buildItem(int i) {
+    return Center(
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(40),
+        child: Container(
+          width: 80,
+          padding: EdgeInsets.all(20),
+          color: Colors.blue[100 * ((i % 8) + 1)],
+          child: Center(
+            child: Text(
+              i.toString(),
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
